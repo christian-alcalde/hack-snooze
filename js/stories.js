613055acb3aecd1b943ef9c8fa6d032a6f9a4c17
Michael Paglione <pagman777@gmail.com>
2022-03-17 22:46:31 -0400

    fix FA link and icon

--- js/stories.js
+++ js/stories.js
@@ -25,7 +25,7 @@ function generateStoryMarkup(story) {
   const hostName = story.getHostName();
   return $(`
       <li id="${story.storyId}">
-      <span><i class="fa-thin fa-star"></i></span>
+      <span><i class="fa-regular fa-star"></i></span>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
